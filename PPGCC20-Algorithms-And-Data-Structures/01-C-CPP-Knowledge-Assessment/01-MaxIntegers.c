afb16320cc78e0f3e4559e03d4da4e85e46c4e72
Breno Farias da Silva <breno.vicio78@gmail.com>
2025-03-26 15:58:32 -0300

    FEAT: Adding the 01 - MaxIntegers PPGCC20 - Algorithms and Data Structures Code

---
+++ PPGCC20-Algorithms-And-Data-Structures/01-C-CPP-Knowledge-Assessment/01-MaxIntegers.c
@@ -0,0 +1,16 @@
+// 1. Escreva uma função em C que receba três números inteiros como parâmetros e retorne o maior deles.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int biggest(const int a, const int b, const int c) {
+   return (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c); // Operador ternário
+}
+
+int main() {
+   int x = 10, y = 25, z = 15;
+
+   printf("O maior número é: %d\n", biggest(x, y, z));
+
+   return 0;
+}
