5130e21db15ffab8894d329e657843a9e1f83eec
Breno Farias da Silva <breno.vicio78@gmail.com>
2025-03-26 16:29:26 -0300

    FEAT: Adding the 04 - Count Words In File in PPGCC20 - Algorithms and Data Structures Code

---
+++ PPGCC20-Algorithms-And-Data-Structures/01-C-CPP-Knowledge-Assessment/04-CountWordsInFile.c
@@ -0,0 +1,43 @@
+// 4. Escreva uma função em C que receba um arquivo de texto como parâmetro e conte o número de palavras que começam com a letra 'a'.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void lerArquivo(const char* file_name) {
+   FILE* fptr = fopen(file_name, "r");
+
+   if (fptr == NULL) {
+      printf("Erro ao abrir o arquivo.\n");
+      return;
+   }
+
+   char ch;
+   int count = 0, inWord = 0;
+
+   while ((ch = fgetc(fptr)) != EOF) {
+      if (ch >= 'A' && ch <= 'Z') {
+         ch += 32; // Adiciona 32 para converter de maiúsculo para minúsculo
+      }
+
+      // Verifica se o caractere é alfabético
+      if ((ch >= 'a' && ch <= 'z')) {
+         if (!inWord) {
+               inWord = 1; // Estamos começando uma palavra
+               if (ch == 'a') {
+                  count++; // A palavra começa com 'a'
+               }
+         }
+      } else {
+         inWord = 0; // Caractere não alfabético, fim de uma palavra
+      }
+   }
+
+   fclose(fptr);
+
+   printf("Número de palavras que começam com 'a': %d\n", count);
+}
+
+int main() {
+   lerArquivo("file.txt");
+   return 0;
+}
