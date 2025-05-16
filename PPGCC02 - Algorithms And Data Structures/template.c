0a4f6de7fb2cd0353ad4b2d384689e15882d0b53
Breno Farias da Silva <breno.vicio78@gmail.com>
2025-05-16 10:41:39 -0300

    REFACTOR: Improving the Template Code for the Algorithm and Data Structure Directory

--- PPGCC02 - Algorithms And Data Structures/template.c
+++ PPGCC02 - Algorithms And Data Structures/template.c
@@ -1,20 +1,20 @@
 /*
- * Descrição: .
- * Autor: Breno Farias da Silva.
- * Data: 27/03/2025.
+ * Description: .
+ * Author: Breno Farias da Silva.
+ * Date: 27/03/2025.
  */
 
 // Compile: gcc $filename.c -o $filenameWithoutExtension
 // Run: ./filenameWithoutExtension
 
-#include <stdio.h> // Printf, scanf
+#include <stdio.h> // printf, scanf
 
 /*
- * Função principal do programa.
- * argc: quantidade de argumentos passados na chamada do programa.
- * argv: vetor com os argumentos passados na chamada do programa.
- * retorno: status de execução do programa (0: sem erros, caso contrário, código de erro).
-*/
+ * Main function of the program.
+ * argc: number of arguments passed on program call.
+ * argv: array with the arguments passed on program call.
+ * return: program execution status (0: no errors, otherwise: error code).
+ */
 int main(int argc, char *argv[]) {
 	printf("Hello World!");
 
